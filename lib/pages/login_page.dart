46e5e011a11e36e9d5d6a988574ac854acd85b13
Mayuresh Samant <mayureshsamant05@gmail.com>
2022-02-28 10:01:02 +0530

    Main2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,111 +3,133 @@
 import 'package:flutter/material.dart';
 import 'package:myfinalapp/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.black,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(190),
-                child: Image.asset(
-                  "assets/images/img1.png",
-                  fit: BoxFit.cover,
-                  height: 200,
-                  width: 210.0,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
                 ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Text(
-                "Login Here",
-                style: TextStyle(color: Colors.blue, fontSize: 60),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Column(
-                children: [
-                  TextFormField(
-                    style: TextStyle(
-                      color: Colors.red,
-                    ),
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.blue.shade300, width: 2.0),
-                      ),
-                      hintText: "Enter Username",
-                      hintStyle: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 15,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(190),
+                  child: Image.asset(
+                    "assets/images/img1.png",
+                    fit: BoxFit.cover,
+                    height: 200,
+                    width: 210.0,
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.blue.shade300, width: 2.0),
-                      ),
-                      hintText: "Enter Password",
-                      hintStyle: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 15,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 300,
-                    height: 65,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          shadowColor: Colors.blue[900],
-                          primary: Colors.black, //background color of button
-                          side: BorderSide(
-                              width: 2,
-                              color:
-                                  Colors.blueAccent), //border width and color
-                          elevation: 3, //elevation of button
-                          shape: RoundedRectangleBorder(
-                              //to set border radius to button
-                              borderRadius: BorderRadius.circular(30)),
-                          padding:
-                              EdgeInsets.all(20) //content padding inside button
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  "Login Here",
+                  style: TextStyle(color: Colors.blue, fontSize: 60),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2.0),
                           ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: Text("Login", style: TextStyle(fontSize: 20)),
-                    ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blue.shade300, width: 2.0),
+                          ),
+                          hintText: "Enter Username",
+                          hintStyle: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 15,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blue.shade300, width: 2.0),
+                          ),
+                          hintText: "Enter Password",
+                          hintStyle: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 15,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Material(
+                        color: Colors.blue,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 25 : 10),
+                        child: InkWell(
+                          onTap: () async {
+                            setState(() {
+                              changeButton = true;
+                            });
+                            await Future.delayed(Duration(seconds: 1));
+                            await Navigator.pushNamed(
+                                context, MyRoutes.homeRoute);
+                            setState(() {
+                              changeButton = false;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeButton ? 50 : 200,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(Icons.done, color: Colors.white)
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ));
   }
